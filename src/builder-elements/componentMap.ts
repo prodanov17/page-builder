3192ef1c26221fd9314c4a5067439faae5c6b7ac
Trajche Prodanov <trajce.p@live.com>
2025-05-18 17:58:30 +0200

    added component prop map

--- src/builder-elements/componentMap.ts
+++ src/builder-elements/componentMap.ts
@@ -11,3 +11,80 @@ export const componentMap = {
   container: ContainerElement,
   // input: InputElement,
 };
+
+export const componentPropsMap = {
+  button: [
+    {
+      name: "width",
+      label: "Width",
+      type: "string",
+    },
+    {
+      name: "height",
+      label: "Height",
+      type: "string",
+    },
+  ],
+  text: [
+    {
+      name: "fontSize",
+      label: "Font Size",
+      type: "string",
+    },
+    {
+      name: "color",
+      label: "Text Color",
+      type: "string",
+    },
+    {
+      name: "content",
+      label: "Content",
+      type: "string",
+    },
+  ],
+  image: [
+    {
+      name: "src",
+      label: "Image URL",
+      type: "string",
+    },
+    {
+      name: "altText",
+      label: "Alt Text",
+      type: "string",
+    },
+    {
+      name: "width",
+      label: "Width",
+      type: "string",
+    },
+    {
+      name: "height",
+      label: "Height",
+      type: "string",
+    },
+  ],
+
+  container: [
+    {
+      name: "backgroundColor",
+      label: "Background Color",
+      type: "string",
+    },
+    {
+      name: "padding",
+      label: "Padding",
+      type: "string",
+    },
+    {
+      name: "minHeight",
+      label: "Min Height",
+      type: "string",
+    },
+    {
+      name: "flexDirection",
+      label: "Flex Direction",
+      type: "string",
+    },
+  ],
+};
